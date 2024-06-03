fec4be4c411b4af30388e49ded754745c1216870
BirWri <55280458+BirWri@users.noreply.github.com>
2024-06-03 17:35:48 +0200

    new_question function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import Quizbrain
 
 question_bank = []
 for question in question_data:
@@ -7,3 +8,8 @@ for question in question_data:
     answer = question['answer']
     new_question = Question(text, answer)
     question_bank.append(new_question)
+
+quiz_brain = Quizbrain(question_bank)
+user_guess = quiz_brain.new_questions()
+print(user_guess)
+
--- quiz_brain.py
+++ quiz_brain.py
@@ -4,5 +4,9 @@ class Quizbrain:
         self.question_number = 0
         self.questions_list = questions_list
 
+    def new_questions(self):
+        current_question = self.questions_list[self.question_number].text
+        user_answer = input(f"Q.{self.question_number + 1}: {current_question} (True/False)?: ")
+        return user_answer
 
 
